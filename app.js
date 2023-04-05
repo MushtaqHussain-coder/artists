c2fe2e97670a098e262c259c38c6b52e28599184
Mushtaq <immushtaq123@gmail.com>
2023-04-05 11:39:00 +0200

    Add cors headers to allow all origins

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(cors);
+
+// enable CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.listen(port, () => {
   console.log(`Node App is listening on ${port}`);
 });
